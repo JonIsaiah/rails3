ac7c966a94df10c46778786c0a32bc6b42ec77e1
Stray Voltage <earthworm.jim117@gmail.com>
2016-07-21 02:50:26 +0000

    added testing for partials to integration test

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -9,6 +9,10 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     get root_path
    # assert_template layout: "application"
     assert_template "static_pages/home"
+    assert_template partial: "_default_head"
+    assert_template partial: "_shim"
+    assert_template partial: "_header"
+    assert_template partial: "_footer"
     assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
