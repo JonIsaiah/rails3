28b4c0a700b673a3b41424bfd3c5180d0f1b9a06
Stray Voltage <earthworm.jim117@gmail.com>
2016-07-21 02:45:54 +0000

    removed the slash that broke the integration tests

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -7,7 +7,8 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   
   test "layout links" do
     get root_path
-    assert_template '/static_pages/home'
+   # assert_template layout: "application"
+    assert_template "static_pages/home"
     assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
