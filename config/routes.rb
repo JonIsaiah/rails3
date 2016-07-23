a1af01865dd388bf9db5028750601e58f238f771
Stray Voltage <earthworm.jim117@gmail.com>
2016-07-23 04:00:43 +0000

    added basic user show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/signup', to: 'users#new'
 
   get '/home', to: 'static_pages#home'
@@ -8,8 +9,8 @@ Rails.application.routes.draw do
   get '/about', to: 'static_pages#about'
   
   get '/contact', to: 'static_pages#contact'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   root 'static_pages#home'
+  
+  resources :users
 end
