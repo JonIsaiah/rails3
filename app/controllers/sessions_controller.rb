c1dbf22b446297e30a0c0203c244c9489ef8c2fe
Stray Voltage <earthworm.jim117@gmail.com>
2016-07-24 22:53:43 +0000

    added handling of invalid login data and added integration tests to guard against regressions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,19 @@
 class SessionsController < ApplicationController
+
   def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && authenticate(params[:session][:password])
+      #login and display user#show
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+  
+  def destroy
+    
   end
 end
