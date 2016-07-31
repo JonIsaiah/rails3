919d32cba4fd72c46f78640b3a576f442e00fd06
Stray Voltage <earthworm.jim117@gmail.com>
2016-07-31 03:27:12 +0000

    added methods to support persistant cookies and retreival of login session

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   
+  attr_accessor :remember_token
+  
   before_save { self.email.downcase! }
   
   validates(:name, presence: true, length: {maximum: 50})
@@ -17,6 +19,19 @@ class User < ApplicationRecord
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
+  
+  def User.new_token        #class << self format can be used 
+    SecureRandom.urlsafe_base64     #rails tutorial ch 9
+  end
+  
+  def User.remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token) )
+  end
+  
+  def authenticated?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
   
 end
